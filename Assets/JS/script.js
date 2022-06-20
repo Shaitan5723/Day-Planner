ea119224cbaaf2451a436adb2479f944a64fe1ec
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-06-20 21:25:54 +1000

    moment.js time
    Added functions to compare current time to app time. Current time affects the color of the background of the divs in the app

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,28 @@
 var now = moment();
-//var nowTime = (toString(now));//
 var currentDay = $('#currentDay')
 var lead = $('.lead')
+var hourBlock = $('.time-block')
 
 currentDay.text(moment().format('MMMM Do YYYY'));
 lead.append(currentDay)
+
+var workTime = moment().startOf("day").add(8, "hour");
+var hour = moment().format("H");
+
+for (var i = 9; i < 18; i++) {
+  var timeSlot = workTime.add(1, "hour").format("HH A");
+  var currentTime;
+
+    if (hour < i) {
+      currentTime = 'past';
+    } else if (hour == i) {
+      currentTime = 'present';
+    } else if (hour > i) {
+      currentTime = 'future';
+    }
+
+    hourBlock
+
+}
+
+
