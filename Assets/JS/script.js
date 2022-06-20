7e397c5e2ec5bfdd623a6e7a880736f3ef16e767
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-06-20 23:15:28 +1000

    JS create containers + buttons
    Moved bootstrap container code into JS to create each container based on the value of i (each hour of the workday).
    
    Added buttons to store and delete data in local storage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,27 +2,56 @@ var now = moment();
 var currentDay = $('#currentDay')
 var lead = $('.lead')
 var hourBlock = $('.time-block')
+var del = $('.fas fa-trash-alt')
+var add = $('saveBtn')
 
+
+//Adds current date to the header display//
 currentDay.text(moment().format('MMMM Do YYYY'));
 lead.append(currentDay)
 
+//Sets the time of the workday to start at 9 AM of the day in question//
 var workTime = moment().startOf("day").add(8, "hour");
-var hour = moment().format("H");
+var currentHour = moment().hour();
 
+//Sets value 'i' as each hour of the work day//
 for (var i = 9; i < 18; i++) {
-  var timeSlot = workTime.add(1, "hour").format("HH A");
+  var timeBlock = workTime.add(1, "hour").format("h A");
   var currentTime;
 
-    if (hour < i) {
-      currentTime = 'past';
-    } else if (hour == i) {
-      currentTime = 'present';
-    } else if (hour > i) {
+  //sets timeblocks to respond to i depending on their relation//
+    if (currentHour < i) {
       currentTime = 'future';
+    } else if (currentHour == i) {
+      currentTime = 'present';
+    } else if (currentHour > i) {
+      currentTime = 'past';
     }
 
-    hourBlock
+    var createContainers = 
+    `<container class="row" id='${i}'>
+        <div class="hour col-2">${timeBlock}</div>
+        <textarea class="event col-8 ${currentTime}"></textarea>
+        <div class="col-1">
+        <button type="submit" class="saveBtn">Save</button>
+        </div>
+        <div class="col-1">
+        <i class="fas fa-trash-alt"> </i>
+        </div>
+    </container>`
+    ;
+
+$(".container-fluid").append(createContainers);
 
 }
 
+var eventContent = $(this).siblings('.event').val();
+var eventTime = $(this).parent().attr('id');
 
+add.click(function() {
+  localStorage.setItem(eventTime,eventContent);
+});
+
+del.click(function() {
+  localStorage.removeItem(eventTime,eventContent);
+});
